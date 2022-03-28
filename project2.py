fc7c05b5b8a8d00dec0b4e98c33d0eada4cdccb6
BMerkosky <bamerkos@ualberta.ca>
2022-03-28 13:31:51 -0600

    Added the addMoviePeople() function

--- project2.py
+++ project2.py
@@ -16,6 +16,50 @@ titlePrincipals = db["title_principals"]
 titleRatings = db["title_ratings"]
 
 # Now we can run our code here. TODO: Create a main function etc.
+def addMoviePeople(titleBasics, nameBasics, titlePrincipals):
+    # Adds a cast/crew member to the title_principals collection
+    validAnswer = False
+    while not validAnswer:
+        # Get the cast member id
+        nconst = input("Provide the id of the person to add: ")
+        result = nameBasics.count_documents({"nconst": nconst})
+        if result > 0:
+            validAnswer = True
+        else:
+            print("Sorry, we could not find a cast/crew member in name_basics with that nconst.")
+    validAnswer = False
+    while not validAnswer:
+        # Get the movie they have acted in
+        tconst = input("Provide the id of the title this person was in: ")
+        result = titleBasics.count_documents({"tconst": tconst})
+        if result > 0:
+            validAnswer = True
+        else:
+            print("Sorry, we couldn't find a movie int title_basics with that tconst.")
+
+    # Get the category
+    category = input("Provide the category (ex: 'production_designer') of this role: ")
+
+    # Get the ordering
+    ordering = titlePrincipals.find_one({"tconst":tconst}, sort=[('ordering', pymongo.DESCENDING)])
+    if ordering == None:
+        # Title not in title_principals
+        ordering = 1
+    else:
+        # Add one to the max
+        ordering = ordering['ordering'] + 1
+        
+
+    # Add the cast/crew member
+    titlePrincipals.insert_one(
+        {"tconst": tconst,
+        "ordering": ordering,
+        "nconst": nconst,
+        "category": category,
+        "job": "\\N",
+        "characters": ["\\N"]})
+
+    print("Cast/crew member added")
 
 
 
